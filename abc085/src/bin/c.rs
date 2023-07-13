1e5f9715a0b002c7a43ee6e0bfdf7f37fb617a9b
00121kuri <00121kuri@gmail.com>
2023-07-14 00:50:17 +0900

    abc085c

--- abc085/src/bin/c.rs
+++ abc085/src/bin/c.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        y: i32,
+    }
+
+    for i in 0..=n {
+        for j in 0..=n-i {
+            let k = n - i - j;
+            if 10000*k + 5000*j + 1000*i == y {
+                println!("{} {} {}", k, j, i);
+                return;
+            }
+            
+        }
+    }
+    println!("-1 -1 -1");
 }
